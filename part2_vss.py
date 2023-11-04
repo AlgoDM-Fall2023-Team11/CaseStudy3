2fe20d8b92831f32f4bc77b6691c13fd9db289fd
Usashiroy <69983754+Usashiroy@users.noreply.github.com>
2023-11-04 08:21:31 +0000

    changes

--- part2_vss.py
+++ part2_vss.py
@@ -1,91 +1,9 @@
-# import streamlit as st
-# import tensorflow as tf
-# import cv2
-# import numpy as np
-
-# # Load the pre-trained model from the H5 file
-# model = tf.keras.models.load_model('visual_search.h5')
-
-# # Function to preprocess the uploaded image
-# def preprocess_image(image):
-#     image = cv2.resize(image, (224, 224))
-#     image = image / 255.0  # Normalize the image
-#     return image
-
-# # Main Streamlit app
-# def main():
-#     st.title("Image Classification App")
-#     uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-#     if uploaded_image is not None:
-#         # Process the uploaded image
-#         image = cv2.imdecode(np.fromstring(uploaded_image.read(), np.uint8), 1)
-#         image = preprocess_image(image)
-
-#         # Make predictions using the loaded model
-#         class_probs = model.predict(np.expand_dims(image, axis=0))[0]
-
-#         # Display the image
-#         st.image(image, caption="Uploaded Image", use_column_width=True)
-
-#         # Display class probabilities
-#         st.write("Class Probabilities:")
-#         for i, prob in enumerate(class_probs):
-#             st.write(f"Class {i}: {prob:.2%}")
-
-# if __name__ == "__main__":
-#     main()
-# import streamlit as st
-# import tensorflow as tf
-# import cv2
-# import numpy as np
-
-# # Load the pre-trained model from the H5 file
-# model = tf.keras.models.load_model('visual_search.h5')
-
-# # Function to preprocess the uploaded image
-# def preprocess_image(image):
-#     image = cv2.resize(image, (224, 224))
-#     image = image / 255.0  # Normalize the image
-#     return image
-
-# # Main Streamlit app
-# def main():
-#     st.title("Image Classification App")
-#     uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
-
-#     if uploaded_image is not None:
-#         # Process the uploaded image
-#         image = cv2.imdecode(np.frombuffer(uploaded_image.read(), np.uint8), 1)
-#         image = preprocess_image(image)
-
-#         # Make predictions using the loaded model
-#         class_probs = model.predict(np.expand_dims(image, axis=0))[0]
-
-#         # Display the image
-#         st.image(image, caption="Uploaded Image", use_column_width=True)
-
-#         # Display class probabilities
-#         st.write("Class Probabilities:")
-#         for i, prob in enumerate(class_probs):
-#             st.write(f"Class {i}: {prob:.2%}")
-
-#         # Debugging info
-#         st.write("Debugging Info:")
-#         st.write(f"Class Labels (in the order used for the model):")
-#         st.write(model.classes_)  # Print the class labels used by the model
-
-# if __name__ == "__main__":
-#     main()
-
-
-
 import streamlit as st
 import tensorflow as tf
 from PIL import Image
 import numpy as np
 
-st.title('Image Classification by Search')
+st.title('Image Classification by Product')
 
 # Load the model
 model = tf.keras.models.load_model('vis_search.h5')
